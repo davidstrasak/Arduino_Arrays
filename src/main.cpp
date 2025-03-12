38562ebaadbba4ab0f912ba02640d40fbf06ff5f
davidstrasak <davidstrasak@seznam.cz>
2025-03-12 20:08:35 +0100

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,67 @@
+#include <Arduino.h>
+
+// lecture 1 and 2
+int sumFunction(int, int);
+int my_integers[6] = { 1,2,3,4,5 };
+char my_char[6] = { 'a', 'b', 'c', 'd', 'e' };
+
+// lecture 3
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+
+  while (!Serial) {
+
+  }
+
+  Serial.println("Your array numbers:");
+  for (int i = 0; i < 5; i++) {
+    Serial.print("Index: ");
+    Serial.print(i);
+    Serial.print(" Value: ");
+    Serial.println(my_integers[i]);
+  }
+
+  Serial.println();
+  Serial.println("Let's calculate those numbers:");
+  int calculated_ints[4];
+
+  for (int i = 0; i < 4; i++) {
+    Serial.print(my_integers[i]);
+    Serial.print(" + ");
+    Serial.print(my_integers[i + 1]);
+    Serial.print(" = ");
+    calculated_ints[i] = sumFunction(my_integers[i], my_integers[i + 1]);
+    Serial.println(calculated_ints[i]);
+
+    my_integers[i + 1] = calculated_ints[i];
+  }
+
+
+  // Lecture 2
+  Serial.println();
+  Serial.println("Your array characters:");
+  for (int i = 0; i < 5; i++) {
+    Serial.print("Index: ");
+    Serial.print(i);
+    Serial.print(" Value: ");
+    Serial.print(my_char[i]);
+    Serial.print(", ");
+    Serial.print("ASCII Value: ");
+    Serial.println((int)my_char[i]);
+  }
+
+  // Lecture 3
+
+}
+
+void loop() {
+
+}
+
+// put function definitions here:
+int sumFunction(int x, int y) {
+  return x + y;
+}
